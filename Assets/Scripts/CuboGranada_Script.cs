c2f28f8a13b531a04a068501c4afbb5031401bb8
Carlosss3 <carlosrojasfeixas@gmail.com>
2015-02-16 19:17:03 +0100

    #245 Implementacion Cubos
    Comienzo Scripts

---
+++ Assets/Scripts/CuboGranada_Script.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CuboGranada_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		
+		if (col.transform.tag == "Player") {
+
+						GameControl.NumGranadas++;
+						Destroy (gameObject);
+						Debug.Log (GameControl.NumGranadas);
+						//	GameControl.CuboGranada--;
+
+				}
+
+		}
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameControl {
+	
+	public static int CuboTele = 0;
+	public static int CuboComb = 0;
+	public static int CuboGranada = 0;
+	public static int NumGranadas = 0;
+	public static int CuboTime = 0;
+	public static int CuboSupernova = 0;
+	
+	
+	
+}
