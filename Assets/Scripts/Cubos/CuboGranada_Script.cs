2eb5ce917c3101ed89c22d1121740c27300658f3
Carlosss3 <carlosrojasfeixas@gmail.com>
2015-02-18 20:19:31 +0100

    #245 Script CuboGranadas

--- Assets/Scripts/Cubos/CuboGranada_Script.cs
+++ Assets/Scripts/Cubos/CuboGranada_Script.cs
@@ -3,26 +3,53 @@ using System.Collections;
 
 public class CuboGranada_Script : MonoBehaviour {
 
+	public GameObject cuboGranada;
+	Vector3 pos;
+	public int maxposx = 853;
+	public int minposx = -1116;
+	public int maxposy = 335;
+	public int minposy = 12;
+	public int maxposz = 1421;
+	public int minposz = -559;
+
+	bool creaCubo;
+	int cubosEscena = 1;
+	//static int maxGranadas = GameControl.maxGranadas;
+	public int maxGranadas = 5;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	void Start() { 
+		//int maxGranadas = GameControl.maxGranadas;
+		//int cubosEscena = GameControl.cubosGranadas;
+
+		}
 	// Update is called once per frame
 	void Update () {
 
 
-	
+		creaCubo = (cubosEscena < maxGranadas); 		//Condicional para crear cubo
+
+
+		if (cubosEscena < maxGranadas) {
+			pos = new Vector3 (Random.Range (maxposx, minposx), Random.Range (maxposy, minposy), Random.Range (maxposz, minposz));
+			Instantiate (cuboGranada, pos, Quaternion.identity);
+			GameControl.cubosGranadas++;
+			cubosEscena++;
+		}
 	}
 
 	void OnTriggerEnter(Collider col){
 		
 		if (col.transform.tag == "Player") {
 
-						GameControl.numGranadas++;
-						Destroy (gameObject);
-						Debug.Log ("Granadas" + GameControl.numGranadas);
-						//	GameControl.CuboGranada--;
+			Destroy (gameObject);			
+			GameControl.numGranadas++;
+			GameControl.cubosGranadas--;
+			cubosEscena--;
+			Debug.Log ("Granada Recogida");
+			Debug.Log ("Granadas: " + GameControl.numGranadas);
+			Debug.Log ("CubosGranada en Escena: " + GameControl.cubosGranadas);
+						
 
 				}
 
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -5,8 +5,11 @@ public static class GameControl {
 	
 	public static int cuboTele = 0;
 	public static int cuboComb = 0;
-	public static int cubosGranadas = 0;
-	public static int numGranadas = 0;
+
+	public static int maxGranadas = 5;		//numero maximo de granadas
+	public static int cubosGranadas = 1;	//Cubos en la escena - Crearemos cubos cuando < maxGranadas
+	public static int numGranadas = 0;		//numero de granadas disponibles
+
 	public static int cuboTime = 0;
 	public static int cuboSupernova = 0;
 	
