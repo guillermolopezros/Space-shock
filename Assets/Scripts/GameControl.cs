3f2499953372bb4ae23d4a981da47a6505e9fc4a
guillermolopezros <guillermolopezros@gmail.com>
2015-02-25 17:47:06 +0100

    sincro

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public static class GameControl {
 	
 	public static int cuboTele = 0;
@@ -12,7 +13,19 @@ public static class GameControl {
 
 	public static int cuboTime = 0;
 	public static int cuboSupernova = 0;
+	public static GameControl gamecontrol;
 	
+
+	void Awake(){
+		if (gamecontrol == null) {
+			gamecontrol = this;
+			DontDestroyOnLoad (gameObject);
+		} else if (gamecontrol != this) {
+			Destroy (this);
+		}
+	}
 	
-	
+	public void Level2(){
+		Application.LoadLevel ("Terreno");
+	}
 }
