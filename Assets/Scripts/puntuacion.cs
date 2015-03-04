6db155a7c9f115e381f264491a3134d00fdba114
guillermolopezros <guillermolopezros@gmail.com>
2015-03-04 18:32:19 +0100

    Puntuacion_canvas

---
+++ Assets/Scripts/puntuacion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class puntuacion : MonoBehaviour {
+
+	Text texto;
+
+	void Start(){
+		texto = gameObject.GetComponent<Text> ();
+	}
+
+	void Update(){
+		texto.text = Mathf.Round(Time.time).ToString ();
+	}
+
+}
+
+
